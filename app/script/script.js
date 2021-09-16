ee0ac0080d5c8f4473b91385db222b08bc09b598
mriliya <mr.litvishchenko@gmail.com>
2021-09-16 10:39:55 +0300

    add tabs feature

--- app/script/script.js
+++ app/script/script.js
@@ -1,14 +1,20 @@
+const body = document.querySelector('body')
 const headerTop = document.querySelector('.header-top')
 const headerTopClose = document.querySelector('.header-top__btn')
+/* popup */
 const loginBtn = document.querySelector('#login')
 const loginPopup = document.querySelector('.header-popup')
-const tabItem = document.querySelectorAll('.main-tab')
-const modalWindow = document.querySelector('.modal')
+/* menu */
 const menuBtn = document.querySelector('.header-burger')
 const menuList = document.querySelector('.header-nav__list')
+/* form */
+const modalWindow = document.querySelector('.modal')
 const submitBtn = document.querySelector('.modal-form__btn')
 const formInputs = document.querySelectorAll('.modal-form__input')
 const phone = document.querySelector('#phone')
+/* tab */
+const tabItem = document.querySelectorAll('.main-tab')
+
 
 /* close header top  */
 headerTopClose.addEventListener('click', () => {
@@ -21,11 +27,22 @@ loginBtn.addEventListener('click', () => {
 })
 
 
-/* show modal window */
+/*open/close tabs & show modal window */
 tabItem.forEach((elem) => {
+	const tabName = elem.querySelector('.main-tab__name')
+	const tabContent = elem.querySelector('.main-tab__content')
+
 	elem.addEventListener('click', (e) => {
 		if (e.target.classList.contains('add__btn')) {
 			modalWindow.classList.add('show')
+			body.classList.add('lock')
+		}
+		if (e.target === tabName ||
+			e.target.classList.contains('main-tab__open') ||
+			e.target.classList.contains('main-tab__name-text')) {
+
+			tabName.classList.toggle('active')
+			tabContent.classList.toggle('show')
 		}
 	})
 })
@@ -34,6 +51,7 @@ tabItem.forEach((elem) => {
 modalWindow.addEventListener('click', (e) => {
 	if (e.target.classList.contains('modal')) {
 		modalWindow.classList.remove('show')
+		body.classList.remove('lock')
 	}
 })
 
@@ -45,24 +63,28 @@ submitBtn.addEventListener('click', (e) => {
 	formInputs.forEach((elem) => {
 		if (elem.value === '' || elem.value === ' ') {
 			elem.placeholder = 'Заполните поле!'
-			elem.style.borderColor='#EC3C24'
+			elem.style.borderColor = '#EC3C24'
 		} else {
-			elem.style.borderColor='#9BAAAE'
+			elem.style.borderColor = '#9BAAAE'
 		}
 	})
 
 	if (!number.test(phone.value)) {
-		phone.style.borderColor='#EC3C24'
+		phone.style.borderColor = '#EC3C24'
 	} else {
 		phone.style.borderColor = '#9BAAAE'
-		modalWindow.classList.remove('show')
 	}
 })
 
 /* show/hide mobile menu */
 menuBtn.addEventListener('click', () => {
 	showElement(menuList)
+	menuBtn.classList.toggle('active')
+	body.classList.toggle('lock')
 })
+
+/* hide/show tabs */
+
 
 /* add/remove class function */
 const showElement = (elem) => {
